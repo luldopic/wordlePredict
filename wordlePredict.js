acc33b402d1144fc0267b45c1327765366c93d66
luldopic <27344798+luldopic@users.noreply.github.com>
2023-01-20 11:04:36 -0500

    Adding generateGuessKey function

--- wordlePredict.js
+++ wordlePredict.js
@@ -23,10 +23,42 @@ function intersection(setA, setB){
     let intersect = new Set([...a].filter(i => b.has(i)));
     return intersect
 }
+
 function compareKeys(guess,target){
     guessKey = wordKey(guess)
     targetKey = wordKey(target)
     correctKeys = intersection(guessKey,targetKey)
     incorrectKeys = guessKey - targetKey
     return [correctKeys,incorrectKeyss]
+}
+
+function generateGuessKeys(guess,green,orange,grey){
+    correctKeys = new Set()
+    wrongKeys = new Set()
+
+    for (i = 0; i<5; i++){
+        if (green[i] == true){
+            correctKeys.add(guess[i])
+            correctKeys.add([guess[i],i])
+        }
+        if (orange[i]  == true){
+            correctKeys.add(guess[i])
+            wrongKeys.add([guess[i],i])
+        }
+        if (grey[i]==true){
+            wrongKeys.add(guess[i])
+            wrongKeys.add([guess[i],i])
+        }
+    }
+    
+    return [correctKeys,wrongKeys]
+}
+
+function possibleAnswer(keys,possible){
+    AllGuess = new Set()
+    possibleAnswer = new Set()
+    listKey = []
+    for (key in keys){
+        //test out keys in possible
+    }
 }
